9c9d6f0ab4a771b15620c95d36106ed74a0d19a6
Beaskyy <afeezakintobi@nqb8.co>
2023-10-31 20:26:59 +0100

    billboards

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,13 +1,18 @@
-import Container from "@/components/ui/container"
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
 
-const HomePage = () => {
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("bfd9341a-722c-4994-90a1-448395d3f285");
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        Billboard
+        <Billboard data={billboard} />
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
